6824d79169d43e0f9eca6d6df902578faab15712
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-06-04 21:28:05 +0300

    feat: done 25 task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -212,8 +212,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n === 2 || n === 3) return true;
+  if (n <= 1 || n % 2 === 0 || n % 3 === 0) return false;
+  for (let i = 5; i * i <= n; i += 6) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
+  }
+
+  return true;
 }
 
 /**
