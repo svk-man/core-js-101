bef5ae69186a923fed526d6610d0f567c53a1060
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-06-04 21:32:58 +0300

    feat: done 26 task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -237,8 +237,9 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const number = Number(value);
+  return typeof number === 'number' && !Number.isNaN(number) ? number : def;
 }
 
 module.exports = {
