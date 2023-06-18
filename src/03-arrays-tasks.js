d3c10427ff7bed50d1d32b371a0503008df59370
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-06-19 02:45:50 +0300

    feat: done 54 task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -517,8 +517,12 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const set = new Set();
+
+  arr.map((item) => set.add(item));
+
+  return Array.from(set.values());
 }
 
 /**
