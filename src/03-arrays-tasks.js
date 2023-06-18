e5cb6fbae862f7a73df5eddec5360b76c5593766
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-06-19 02:29:24 +0300

    feat: done 51 task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -439,8 +439,26 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr
+    .sort((a, b) => {
+      if (a.city < b.city) {
+        return -1;
+      }
+      if (a.city > b.city) {
+        return 1;
+      }
+      return 0;
+    })
+    .sort((a, b) => {
+      if (a.country < b.country) {
+        return -1;
+      }
+      if (a.country > b.city) {
+        return 1;
+      }
+      return 0;
+    });
 }
 
 /**
