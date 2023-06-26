25a0361230b4b399770e861f58f609ae272bffd0
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-06-27 00:35:00 +0300

    feat: done 57 task

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -599,8 +599,8 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return arr.flat(indexes.length - 1)[indexes[indexes.length - 1]];
 }
 
 
