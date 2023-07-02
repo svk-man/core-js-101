bc05e5ae04400e626ff328623390d776dfd2afe2
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-07-02 21:05:14 +0300

    feat: done 86 task

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-function toNaryString(num, n) {
-  return num.toString(n);
+function getCommonDirectoryPath(pathes) {
+  const changedPathes = pathes.map((path) => path.split('/'));
+
+  const commonPath = [];
+  let isCommonPartPath = true;
+  for (let i = 0; i < changedPathes[0].length && isCommonPartPath; i += 1) {
+    const commonPartPath = changedPathes[0][i];
+    for (let j = 0; j < changedPathes.length && isCommonPartPath; j += 1) {
+      if (commonPartPath !== changedPathes[j][i]) {
+        isCommonPartPath = false;
+      }
+    }
+
+    if (isCommonPartPath) {
+      commonPath.push(commonPartPath);
+    }
+  }
+
+  return commonPath.length ? `${commonPath.join('/')}/` : '';
 }
 
-console.log(toNaryString(1024, 2));
+console.log(getCommonDirectoryPath(['/web/images/image1.png', '/web/images/image2.png']));
--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -405,8 +405,25 @@ function toNaryString(num, n) {
  *   ['/web/assets/style.css', '/.bin/mocha',  '/read.me'] => '/'
  *   ['/web/favicon.ico', '/web-scripts/dump', '/verbalizer/logs'] => '/'
  */
-function getCommonDirectoryPath(/* pathes */) {
-  throw new Error('Not implemented');
+function getCommonDirectoryPath(pathes) {
+  const changedPathes = pathes.map((path) => path.split('/'));
+
+  const commonPath = [];
+  let isCommonPartPath = true;
+  for (let i = 0; i < changedPathes[0].length && isCommonPartPath; i += 1) {
+    const commonPartPath = changedPathes[0][i];
+    for (let j = 0; j < changedPathes.length && isCommonPartPath; j += 1) {
+      if (commonPartPath !== changedPathes[j][i]) {
+        isCommonPartPath = false;
+      }
+    }
+
+    if (isCommonPartPath) {
+      commonPath.push(commonPartPath);
+    }
+  }
+
+  return commonPath.length ? `${commonPath.join('/')}/` : '';
 }
 
 
