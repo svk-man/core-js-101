eab3cf2b32444cd67c55669b6f54443543894b56
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-07-03 00:39:27 +0300

    fix: remove script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function getIdGeneratorFunction(startFrom) {
-  let id = startFrom;
-  return () => id += 1;
-}
-
-const getId4 = getIdGeneratorFunction(4);
-const getId10 = getIdGeneratorFunction(10);
-console.log(getId4());
-console.log(getId10());
